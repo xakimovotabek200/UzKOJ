8f429c9dd2bec241194d786f900075d7018f8eeb
unknown <xakimovotabek200>
2024-03-17 10:20:04 +0500

    add routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import { routes } from "./router/routes";
+import { Link } from "react-router-dom";
+import Login from "./components/Login";
 
 const App = () => {
+  const { pathname } = useLocation();
+  const token = sessionStorage.getItem("token");
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  useLayoutEffect(() => {
+    window.scroll({ left: 0, top: 0 });
+  }, [pathname]);
+
+  if (!isLoggedIn) {
+    return <Login />;
+  }
   return (
     <div>
-      <button className="btn">Hello !!!</button>
+      <Routes>
+        {routes.map((item) => (
+          <Route
+            key={item.key}
+            path={item.path}
+            element={
+              <Link to={item.path}>
+                <div className="card w-96 bg-primary text-primary-content">
+                  <div className="card-body">
+                    <h2 className="card-title">{item.title}!</h2>
+                  </div>
+                </div>
+              </Link>
+            }
+          />
+        ))}
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Documents from "./Documents";
+import Events from "./Events";
+import Members from "./Members";
+import Problems from "./Problems";
+import Registered from "./Registered";
+import Stats from "./Stats";
+import Home from "../components/Home";
+
+export { Documents, Events, Members, Problems, Registered, Stats, Home };
