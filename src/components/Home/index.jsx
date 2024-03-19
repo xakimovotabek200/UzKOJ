ec5f9fb9381acbe846458d646da8df71d5442cd2
unknown <xakimovotabek200>
2024-03-19 16:34:03 +0500

    add card in the home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,51 +1,27 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { routes } from "../../router/routes";
 
 const Index = () => {
   const { pathname } = useLocation();
-  const [activePath, setActivePath] = useState("");
-
-  const isMenuItemActive = (path) => {
-    return activePath === path;
-  };
 
   useLayoutEffect(() => {
-    setActivePath(pathname);
     window.scroll({ left: 0, top: 0 });
   }, [pathname]);
 
   return (
     <div>
-      <ul className="pt-2 w-full grid grid-cols-4 text-center">
+      <ul className="pt-2 w-full grid grid-cols-4 gap-4 text-center">
         {routes.map((Menu, index) => (
           <NavLink
             to={Menu.path}
             key={index}
-            className={`rounded-md p-2 cursor-pointer w-full`}
-            onClick={() => setActivePath(Menu.path)}
+            className="rounded-lg overflow-hidden py-16 shadow-md border border-gray-200 transition duration-300 ease-in-out transform hover:scale-105"
           >
-            <div
-              className="py-5"
-              style={{
-                backgroundColor: isMenuItemActive(Menu.path)
-                  ? "red"
-                  : "whitesmoke",
-              }}
-            >
-              <div className="">
-                <span
-                  className={`text-4xl  ${
-                    isMenuItemActive(Menu.path) ? "text-white" : "text-black"
-                  }`}
-                >
-                  {Menu.icon}
-                </span>
-                <p
-                  className={`text-xl font-semibold pl-4 ${
-                    isMenuItemActive(Menu.path) ? "text-white" : "text-black"
-                  }`}
-                >
+            <div className="py-5">
+              <div className="flex flex-col items-center ">
+                <span className="text-4xl">{Menu.icon}</span>
+                <p className="text-xl font-semibold mt-2 border-t w-full ">
                   {Menu.title}
                 </p>
               </div>
