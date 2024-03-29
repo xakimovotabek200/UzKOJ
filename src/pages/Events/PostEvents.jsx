401bda70050420e7a59fdb9a4c0e195620f91430
unknown <xakimovotabek200>
2024-03-29 15:57:55 +0500

    add last changes for documents

--- src/pages/Events/PostEvents.jsx
+++ src/pages/Events/PostEvents.jsx
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { toast } from "react-toastify";
-import { Button, Text, Translated } from "../../components";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Button, Text, Translated } from "../../components";
+import { useState } from "react";
 
 const index = () => {
   const navigate = useNavigate();
