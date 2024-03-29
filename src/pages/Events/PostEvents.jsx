cdacbac8ad3afa33e9d2d575670128129d38ed45
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-03-29 15:59:30 +0500

    bug fix

--- src/pages/Events/PostEvents.jsx
+++ src/pages/Events/PostEvents.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { toast } from "react-toastify";
-import { Button, Text, Translated } from "../../components";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Button, Text, Translated } from "../../components";
