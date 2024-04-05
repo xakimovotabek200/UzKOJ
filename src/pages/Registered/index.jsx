262a64738521684f66435cc50abc09b7486a8ab6
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-04-05 13:58:59 +0500

    add filtering by region

--- src/pages/Registered/index.jsx
+++ src/pages/Registered/index.jsx
@@ -9,7 +9,9 @@ import DeleteRegisteredUser from "./DeleteRegisteredUser";
 
 const index = () => {
   const [data, setData] = useState(null);
+  const [regions, setRegions] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState({ userId: null });
   const token = sessionStorage.getItem("token");
   const user_id = +sessionStorage.getItem("user_id");
 
@@ -18,6 +20,9 @@ const index = () => {
       .get(`/clients${user_id !== 1 ? `/user/${user_id}` : ""}`)
       .then((res) => setData(res?.data))
       .finally(() => setLoading(false));
+    if (user_id === 1) {
+      await axios.get(`/users`).then((res) => setRegions(res?.data));
+    }
   }
 
   useEffect(() => {
@@ -62,6 +67,31 @@ const index = () => {
           </Link>
         </div>
       </div>
+
+      {/* filter by region */}
+      {user_id === 1 && (
+        <div className="flex items-center gap-2 mt-2">
+          <Text>
+            <Translated>Tuman bo'yicha filterlash:</Translated>
+          </Text>
+          <select
+            onChange={(e) =>
+              setFilter((old) => ({ ...old, userId: e.target.value }))
+            }
+            className="p-3 border border-black/30 rounded"
+          >
+            <option className="italic">
+              <Translated>Tanlanmangan</Translated>
+            </option>
+            {regions?.map((item) => (
+              <option key={item?.id} value={item?.id}>
+                {item?.region}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+
       <div className="overflow-x-auto mt-5">
         <table className="table w-full bg-white text-center border">
           <thead className="border">
@@ -111,45 +141,50 @@ const index = () => {
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            {data?.map?.((item, ind) => (
-              <tr key={ind} className="border">
-                <th className="border p-1">
-                  <Text>{ind + 1}</Text>
-                </th>
-                <td className="border p-1">
-                  <Text>{item?.fullName}</Text>
-                </td>
-                <td className="border p-1">
-                  <Text>{item?.birthDate}</Text>
-                </td>
-                <td className="border p-1">
-                  <Text>{item?.address}</Text>
-                </td>
-                <td className="border p-1">
-                  <Text>{item?.phoneNumber}</Text>
-                </td>
-                <td className="border p-1">
-                  <Text>
-                    {item?.passportSeries + " " + item?.passportNumber}
-                  </Text>
-                </td>
-                <td className="border p-1">
-                  <Text>{item?.state}</Text>
-                </td>
-                <td className="border p-1">
-                  <Text>{item?.groupNumber}</Text>
-                </td>
-                <td className="border p-1">
-                  <Link to={`/edit-user/${item?.id}`} state={item}>
-                    <Button>
-                      <span className="fa-solid fa-edit text-blue-500" />
-                    </Button>
-                  </Link>
-                  <DeleteRegisteredUser item={item} getData={getData} />
-                </td>
-              </tr>
-            ))}
+            {data
+              ?.filter((item) =>
+                filter.userId > 0
+                  ? +item.userId === +filter.userId
+                  : item.userId
+              )
+              ?.map?.((item, ind) => (
+                <tr key={ind} className="border">
+                  <th className="border p-1">
+                    <Text>{ind + 1}</Text>
+                  </th>
+                  <td className="border p-1">
+                    <Text>{item?.fullName}</Text>
+                  </td>
+                  <td className="border p-1">
+                    <Text>{item?.birthDate}</Text>
+                  </td>
+                  <td className="border p-1">
+                    <Text>{item?.address}</Text>
+                  </td>
+                  <td className="border p-1">
+                    <Text>{item?.phoneNumber}</Text>
+                  </td>
+                  <td className="border p-1">
+                    <Text>
+                      {item?.passportSeries + " " + item?.passportNumber}
+                    </Text>
+                  </td>
+                  <td className="border p-1">
+                    <Text>{item?.state}</Text>
+                  </td>
+                  <td className="border p-1">
+                    <Text>{item?.groupNumber}</Text>
+                  </td>
+                  <td className="border p-1">
+                    <Link to={`/edit-user/${item?.id}`} state={item}>
+                      <Button>
+                        <span className="fa-solid fa-edit text-blue-500" />
+                      </Button>
+                    </Link>
+                    <DeleteRegisteredUser item={item} getData={getData} />
+                  </td>
+                </tr>
+              ))}
             {loading && (
               <tr>
                 <td colSpan={9}>
