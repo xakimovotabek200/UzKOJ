b9b39f4172cf22142e2c98644916644fd389d1b3
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-04-24 15:27:26 +0500

    create deadline slice

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import textReducer from "../text";
 import accesibilityReducer from "../accesibility";
+import deadlineReducer from "../deadline";
 
 export const store = configureStore({
   reducer: {
     text: textReducer,
     accesibility: accesibilityReducer,
+    deadline: deadlineReducer,
   },
 });
