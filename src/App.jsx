2e5968104b75cafb8b725aece5cabeafe14f6c9b
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-04-24 16:03:05 +0500

    add checking deadline function

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { Provider } from "react-redux";
-import { store } from "./redux";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { changeDeadlineStatus } from "./redux";
 import Router from "./router/index";
 import Login from "./components/Login";
 
 const App = () => {
+  const dispatch = useDispatch();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
+  const token = sessionStorage.getItem("token");
+
+  async function checkDeadline() {
+    try {
+      const isOkResponse = await axios.get("deadline/1");
+      dispatch(changeDeadlineStatus(isOkResponse.data));
+    } catch (error) {
+      return;
+    }
+  }
 
   useEffect(() => {
     const checkLoginStatus = async () => {
       try {
-        const token = sessionStorage.getItem("token");
         const isValidToken = !!token;
 
         if (isValidToken) {
+          checkDeadline();
           setIsLoggedIn(true);
         } else {
           setIsLoggedIn(false);
@@ -34,11 +46,9 @@ const App = () => {
   }
 
   return (
-    <Provider store={store}>
-      <div>
-        {isLoggedIn ? <Router /> : <Login setIsLoggedIn={setIsLoggedIn} />}
-      </div>
-    </Provider>
+    <div>
+      {isLoggedIn ? <Router /> : <Login setIsLoggedIn={setIsLoggedIn} />}
+    </div>
   );
 };
 
